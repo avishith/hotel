62dba259b23a9bd0483b22abae3b0bd4829e0fac
avishith <49362476+avishith@users.noreply.github.com>
2020-06-26 22:42:03 +0530

    loginpy

---
+++ LOGIN.py
@@ -0,0 +1,6 @@
+def login():
+	logname=input("Enter user name : ")
+	logpass= input("enter your password : ")
+def signin():
+	signame=input("Enter user name : ")
+	signpass=input("Enter user password : ")
